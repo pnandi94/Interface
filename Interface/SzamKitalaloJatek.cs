894ecebffb776389fb3e2c8fe31c05f2445b911a
pnandi94 <74568206+pnandi94@users.noreply.github.com>
2023-09-22 08:58:53 +0200

    Add project files.

---
+++ Interface/SzamKitalaloJatek.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class SzamKitalaloJatek
+    {
+        private const int MAX_VERSENYZO = 5;
+        private ITippelo[] versenyzok = new ITippelo[MAX_VERSENYZO];
+        private int versenyzoN;
+        private int cel;
+
+        public SzamKitalaloJatek(int alsoHatar, int felsoHatar)
+        {
+            this.cel = new Random().Next(alsoHatar, felsoHatar + 1);
+            this.versenyzoN = 0;
+        }
+
+        public void VersenyzoFelvetele(ITippelo jatekos)
+        {
+            if (versenyzoN < MAX_VERSENYZO)
+            {
+                versenyzok[versenyzoN] = jatekos;
+                versenyzoN++;
+            }
+            else
+            {
+                Console.WriteLine("Maximum versenyző szám elérve.");
+            }
+        }
+
+        public void VersenyIndul()
+        {
+            foreach (ITippelo jatekos in versenyzok)
+            {
+                if (jatekos != null)
+                {
+                    jatekos.JatekIndul(cel - 1, cel + 1);
+                }
+            }
+        }
+
+        public bool MindenkiTippel()
+        {
+            bool vanNyertes = false;
+            foreach (ITippelo jatekos in versenyzok)
+            {
+                if (jatekos != null)
+                {
+                    int tipp = jatekos.KovetkezoTipp();
+                    if (tipp == cel)
+                    {
+                        jatekos.Nyert();
+                        vanNyertes = true;
+                    }
+                }
+            }
+
+            if (vanNyertes)
+            {
+                foreach (ITippelo jatekos in versenyzok)
+                {
+                    if (jatekos != null)
+                    {
+                        if (jatekos.KovetkezoTipp() != cel)
+                        {
+                            jatekos.Veszitett();
+                        }
+                    }
+                }
+            }
+
+            return vanNyertes;
+        }
+
+        public void Jatek()
+        {
+            VersenyIndul();
+            while (!MindenkiTippel()) { }
+        }
+    }
+}
