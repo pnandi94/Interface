0f35205e806c83d9de248d1e347d641bf638069a
pnandi94 <74568206+pnandi94@users.noreply.github.com>
2023-09-22 09:03:35 +0200

    1

---
+++ Interface/GepiJatekos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public abstract class GepiJatekos : ITippelo
+    {
+        protected int alsoHatar;
+        protected int felsoHatar;
+        protected int nyertDB;
+        protected int veszitettDB;
+
+        public GepiJatekos()
+        {
+            nyertDB = 0;
+            veszitettDB = 0;
+        }
+
+        public void JatekIndul(int alsoHatar, int felsoHatar)
+        {
+            this.alsoHatar = alsoHatar;
+            this.felsoHatar = felsoHatar;
+        }
+
+        public void Nyert()
+        {
+            nyertDB++;
+        }
+
+        public void Veszitett()
+        {
+            veszitettDB++;
+        }
+
+        public abstract int KovetkezoTipp();
+    }
+}
